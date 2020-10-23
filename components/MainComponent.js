e405558bdd7f259f97e62ff43ef33149a5f32da1
VishakAmin <aminvishak@gmail.com>
2020-10-24 02:13:36 +0530

    Added Basics Components

---
+++ components/MainComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { DISHES } from '../shared/dishes'
+import Menu from './MenuComponent';
+
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        }
+    }
+
+    render() {
+        return (
+            <Menu dishes={this.state.dishes} />
+        )
+    }
+}
+
+export default Main;
